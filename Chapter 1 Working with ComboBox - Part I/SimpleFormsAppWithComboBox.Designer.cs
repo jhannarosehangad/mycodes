147a56517f0a941c094bfa4a7fb4b8f5456f1ca6
Jhanna Rose Hangad <jhannarosehangad82@gmail.com>
2025-03-17 10:17:28 +0800

    Add files via upload

---
+++ Chapter 1 Working with ComboBox - Part I/SimpleFormsAppWithComboBox.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SimpleFormsAppWithComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.movieComboBox = new System.Windows.Forms.ComboBox();
+            this.Select = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.movieCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.movieListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Favorite Movie Picker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Genre";
+            // 
+            // movieComboBox
+            // 
+            this.movieComboBox.FormattingEnabled = true;
+            this.movieComboBox.Location = new System.Drawing.Point(59, 109);
+            this.movieComboBox.Name = "movieComboBox";
+            this.movieComboBox.Size = new System.Drawing.Size(121, 21);
+            this.movieComboBox.TabIndex = 2;
+            this.movieComboBox.Text = "Animation";
+            // 
+            // Select
+            // 
+            this.Select.Location = new System.Drawing.Point(59, 136);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(121, 23);
+            this.Select.TabIndex = 3;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(229, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Movie List";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(232, 316);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(171, 26);
+            this.addBtn.TabIndex = 8;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(459, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "My Favorite Movie List";
+            // 
+            // movieCheckedListBox
+            // 
+            this.movieCheckedListBox.FormattingEnabled = true;
+            this.movieCheckedListBox.Location = new System.Drawing.Point(232, 109);
+            this.movieCheckedListBox.Name = "movieCheckedListBox";
+            this.movieCheckedListBox.Size = new System.Drawing.Size(171, 184);
+            this.movieCheckedListBox.TabIndex = 10;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(655, 126);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(92, 23);
+            this.deleteBtn.TabIndex = 11;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(655, 164);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(92, 24);
+            this.clearBtn.TabIndex = 12;
+            this.clearBtn.Text = "Clear All";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // movieListBox
+            // 
+            this.movieListBox.FormattingEnabled = true;
+            this.movieListBox.Location = new System.Drawing.Point(462, 109);
+            this.movieListBox.Name = "movieListBox";
+            this.movieListBox.Size = new System.Drawing.Size(163, 108);
+            this.movieListBox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.movieListBox);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.movieCheckedListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.movieComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox movieComboBox;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox movieCheckedListBox;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.ListBox movieListBox;
+    }
+}
+
---
+++ Chapter 1 Working with ComboBox - Part I/SimpleFormsAppWithComboBox.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormsAppWithComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] movieGenre = { "Animation", "Horror", "Sci-Fi" };
+            movieComboBox.Items.AddRange(movieGenre);
+
+            movieComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            movieComboBox.SelectedIndex = 0;
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            string[] animatedMovieList = { "Big Hero 6", "Bolt", "Shrek 4" };
+            string[] horrorMovieList = { "Shake Rattle & Roll", "Feng Shui" };
+            string[] scifiMovieList = { "Interstellar", "Oblivion" };
+
+            movieCheckedListBox.Items.Clear();
+
+            if (movieComboBox.SelectedItem.ToString().Contains("Animation"))
+            {
+                movieCheckedListBox.Items.AddRange(animatedMovieList);
+            } else if (movieComboBox.SelectedItem.ToString().Contains("Horror"))
+            {
+                movieCheckedListBox.Items.AddRange(horrorMovieList);
+            } else if (movieComboBox.SelectedItem.ToString().Contains("Sci-Fi"))
+            {
+                movieCheckedListBox.Items.AddRange(scifiMovieList);
+            }
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            foreach ( var item in movieCheckedListBox.Items)
+            {
+                bool isSelected = movieCheckedListBox.GetItemChecked(movieCheckedListBox.Items.IndexOf(item));
+                if (isSelected && !movieListBox.Items.Contains(item))
+                {
+                    movieListBox.Items.Add(item);
+
+                }
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (movieListBox.Items.Count >= 1)
+            {
+                movieListBox.Items.Remove(movieListBox.SelectedItem);
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            movieListBox.Items.Clear();
+        }
+    }
+}
